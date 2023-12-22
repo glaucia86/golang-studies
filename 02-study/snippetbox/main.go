c50479405b3b60376b076e165e0e6e4cdebfc9de
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 12:56:36 -0300

    refactor: main function to use http package for serving a simple response

--- 02-study/snippetbox/main.go
+++ 02-study/snippetbox/main.go
@@ -7,8 +7,21 @@
 
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting the server on port...: 4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
