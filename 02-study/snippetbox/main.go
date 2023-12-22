fc26b5d59696a34d42b202e77381507541d5e283
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 20:59:41 -0300

    chore: customizing headers for 'snippetCreate' handler

--- 02-study/snippetbox/main.go
+++ 02-study/snippetbox/main.go
@@ -30,6 +30,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
 		w.WriteHeader(405)
 		w.Write([]byte("Method not allowed"))
 
