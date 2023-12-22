a99a9cb9198f0befef0891ae5c0d1bb1f9f9817a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 12:23:03 -0300

    chore: Add main.go file with initial code

---
+++ 02-study/snippetbox/main.go
@@ -0,0 +1,14 @@
+/**
+ * file: main.go
+ * description: file responsible for running the application.
+ * data: 22/12/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
