8a90d4e726d30b6fce9363e315b6ea885184eaf5
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 20:54:09 -0300

    fix: HTTP method check in 'snippetCreate' function

--- 02-study/snippetbox/main.go
+++ 02-study/snippetbox/main.go
@@ -16,6 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
+
 		return
 	}
 
@@ -27,6 +28,14 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+
+		return
+	}
+
 	w.Write([]byte("Create a new snippet"))
 }
 
