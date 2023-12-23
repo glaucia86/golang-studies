46afb52ba7489278350014b8aafa8f7bc0ffd1af
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 21:43:34 -0300

    chore: Add 'Content-Type' and 'X-XSS-Protection' headers to 'snippetCreate' handler

--- 02-study/snippetbox/main.go
+++ 02-study/snippetbox/main.go
@@ -31,6 +31,8 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
+		w.Header().Set("Content-Type", "application/json")
+		w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 
 		return
