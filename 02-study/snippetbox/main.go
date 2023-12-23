59c94ca36ceced839de9b4b55f13310807186efb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 21:57:16 -0300

    chore: Add ID validation to 'snippetView' function

--- 02-study/snippetbox/main.go
+++ 02-study/snippetbox/main.go
@@ -8,8 +8,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +26,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet...."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
