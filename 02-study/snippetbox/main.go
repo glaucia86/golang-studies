3f3d5c22521d901909e7bf1e3e6ce7e2d262f5c1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-22 21:13:59 -0300

    refactor: avoid constants in the HTTP method 'snippetCreate' function

--- 02-study/snippetbox/main.go
+++ 02-study/snippetbox/main.go
@@ -29,9 +29,9 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method not allowed", 405)
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 
 		return
 	}
