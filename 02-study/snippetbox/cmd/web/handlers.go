a41cf5612fc6c57750b15c9005f1efcd88cec034
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-24 20:56:35 -0300

    chore: Update 'base.htm'l and 'handlers.go' files

--- 02-study/snippetbox/cmd/web/handlers.go
+++ 02-study/snippetbox/cmd/web/handlers.go
@@ -42,8 +42,6 @@ func home(w http.ResponseWriter, r *http.Request) {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
-
-	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
