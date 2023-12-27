64547e3b9197421f0b9df983723d864e538aa888
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-27 20:22:53 -0300

    chore: Add helper functions for handling server errors, client errors, and not found responses

---
+++ 02-study/snippetbox/cmd/web/helpers.go
@@ -0,0 +1,30 @@
+/**
+ * file: cmd/web/helpers
+ * description: file responsible for running the application.
+ * data: 12/27/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	var (
+		method = r.Method
+		uri    = r.URL.RequestURI()
+	)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
