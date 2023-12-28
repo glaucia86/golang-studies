f3f962a7b5c3f3cfd3dccd72ab8640afb0b9b7c1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-28 10:38:44 -0300

    chore: Add main.go file with initial code

---
+++ 03-study/main.go
@@ -0,0 +1,12 @@
+/**
+ * file: main.go
+ * description: file responsible for running the application.
+ * data: 12/28/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package main
+
+func main() {
+	println("Hello World!")
+}
