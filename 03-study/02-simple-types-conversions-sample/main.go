82c9455e5b585183b2ffa2cd9bcea09ec221aecc
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-28 11:07:19 -0300

    chore: Add simple types and conversions sample code

---
+++ 03-study/02-simple-types-conversions-sample/main.go
@@ -0,0 +1,27 @@
+/**
+ * file: main.go
+ * description: file responsible for running the application.
+ * data: 12/28/2023
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	a := "Glaucia Lemos"
+	fmt.Println(a)
+
+	var b int = 99
+	fmt.Println(b)
+
+	var c = true
+	fmt.Println(c)
+
+	d := 3.1415
+	fmt.Println(d)
+
+	var e int = int(d)
+	fmt.Println(e)
+}
