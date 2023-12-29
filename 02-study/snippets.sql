3dcaa66c1d31caea5bc5e0c3361a3f63fb508736
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-12-29 20:11:45 -0300

    chore: Update MySQL execution instructions and modify go.mod file

--- 02-study/snippets.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE snippets (
-	id SERIAL PRIMARY KEY,
-	title VARCHAR(100) NOT NULL,
-	content TEXT NOT NULL,
-	created TIMESTAMP NOT NULL,
-	experies TIMESTAMP NOT NULL
-);
-
-CREATE INDEX idx_snippets_created ON snippets(created)
