e8546c1dde1a97956a23860a9485e016abae7a21
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-01 20:45:53 -0300

    chore: Update 'main.go' with MySQL database connection

--- 02-study/snippetbox/cmd/web/main.go
+++ 02-study/snippetbox/cmd/web/main.go
@@ -1,17 +1,20 @@
 /**
  * file: cmd/web/main.go
  * description: file responsible for running the application.
- * data: 12/27/2023
+ * data: 01/01/2024
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -21,9 +24,18 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:Br@zil2024@tcp(127.0.0.1:3306)/snippetbox?parseTime=true", "MySQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
@@ -31,7 +43,22 @@ func main() {
 
 	logger.Info("Starting the server on port", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
