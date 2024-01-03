653c80ddfe16f31e644ab40914ad9e8318708858
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-02 22:07:12 -0300

    chore: Add error handling for snippet retrieval and create error package

--- 02-study/snippetbox/internal/models/snippets.go
+++ 02-study/snippetbox/internal/models/snippets.go
@@ -9,6 +9,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -42,7 +43,24 @@ func (m *SnippetModel) Insert(title string, content string, expires int) (int, e
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	return Snippet{}, nil
+
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+						WHERE expires > UTC_TIMESTAMP() AND id = ?`
+
+	row := m.DB.QueryRow(stmt, id)
+
+	var s Snippet
+
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, nil
+		}
+	}
+
+	return s, nil
 }
 
 func (m *SnippetModel) LatestSnippets() ([]Snippet, error) {
