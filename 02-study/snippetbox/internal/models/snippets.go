a924c8878a0f2a98bd3bde853613e749b26d3832
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-02 21:16:11 -0300

    chore: create a logic for the 'SnippetModel.Insert()' method

--- 02-study/snippetbox/internal/models/snippets.go
+++ 02-study/snippetbox/internal/models/snippets.go
@@ -1,7 +1,7 @@
 /**
  * file: internal/models/snippets.go
  * description: file responsible for the model of the application.
- * data: 01/01/2024
+ * data: 01/02/2024
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
@@ -24,8 +24,23 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
+// 02/01/2024 -> exemplo: DATA ATUAL + 365 DIAS
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+						VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
+
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
