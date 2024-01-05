237692ab5012e6e5ebf25b0da73ce1d4e636bd7b
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-04 22:56:32 -0300

    chore: Add slices sample code

---
+++ 03-study/09-slices-sample/main.go
@@ -0,0 +1,35 @@
+/**
+ * file: main.go
+ * description: file responsible for running the application.
+ * data: 01/04/2024
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	var s []string
+	fmt.Println(s) // [  ]
+
+	s = []string{"Coffee", "Espresso", "Capuccino"}
+	fmt.Println(s) // [Coffee Espresso Capuccino]
+
+	s = append(s, "Hot Chocolate", "Hot Tea")
+	fmt.Println(s)
+
+	slices.Delete(s, 1, 2)
+	fmt.Println(s) 
+
+	// fmt.Println(s[1]) // Espresso
+	// s[1] = "Mocha"
+	// fmt.Println(s) // [Coffee Mocha Capuccino]
+
+	// s2 := s
+	// fmt.Println(s, s2)
+
+}
--- 03-study/go.mod
+++ 03-study/go.mod
@@ -1,3 +1,5 @@
 module demo
 
 go 1.21.0
+
+require golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
