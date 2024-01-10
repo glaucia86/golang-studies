352a90c5fbd441a24d0a5ca5e569831c5c6b383a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-10 18:04:44 -0300

    chore: Add map sample code

---
+++ 03-study/10-map-sample/main.go
@@ -0,0 +1,40 @@
+/**
+ * file: main.go
+ * description: file responsible for running the application.
+ * data: 01/04/2024
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	var m map[string][]string
+	fmt.Println(m) // map[]
+
+	m = map[string][]string{
+		"coffee": {"Espresso", "Capuccino", "Mocha"},
+		"tea":    {"Hot Tea", "Chai Tea", "Chai Latte"},
+	}
+
+	fmt.Println(m)           // map[coffee:[Espresso Capuccino Mocha] tea:[Hot Tea Chai Tea Chai Latte]]
+	fmt.Println(m["coffee"]) // [Espresso Capuccino Mocha]
+
+	m["water"] = []string{"Sparkling"}
+	fmt.Println(m) // map[coffee:[Espresso Capuccino Mocha] tea:[Hot Tea Chai Tea Chai Latte] water:[Sparkling]]
+
+	delete(m, "tea")
+	fmt.Println(m) // map[coffee:[Espresso Capuccino Mocha] water:[Sparkling]]
+
+	fmt.Println(m["tea"])
+	v, ok := m["tea"]
+	fmt.Println(v, ok) // [] false
+
+	m2 := m
+	m2["coffee"] = []string{"Coffee"}
+	m["tea"] = []string{"Hot Tea"}
+
+	fmt.Println(m)
+	fmt.Println(m2)
+}
